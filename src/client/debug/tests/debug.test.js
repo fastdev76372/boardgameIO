fastdev76372/boardgameIO

commit d3360e8458074444da64a557375b8ec4f26b708c

feat(debug): Display live object view in debug panel (#781)
* Enforce that the debug menu is always on the top of the screen

* use a json-tree instead of a JSON.stirngify string.

* Update Debug.svelte

* add svelte-json-tree-auto to transform ignore patterns

* add svelte-json-tree

* add svelte-json-tree to transform ignore patterns

* fix(debug): require `svelte-json-tree` instead of importing it

Context: https://github.com/boardgameio/boardgame.io/pull/781#discussion_r473150625

* chore(debug): Use `svelte-json-tree-auto`

* refactor(debug): Clean up JSON tree styles

Co-authored-by: Chris Swithinbank <swithinbank@gmail.com>

src/client/debug/main/Main.svelte
@@ -1,6 +1,7 @@
 <script>
   export let client;
 
+  const JSONTree = require('svelte-json-tree-auto');
   import Move from './Move.svelte';
   import Controls from './Controls.svelte';
   import PlayerInfo from './PlayerInfo.svelte';
@@ -31,9 +32,9 @@
 </script>
 
 <style>
-  .json {
-    font-family: monospace;
-    color: #888;
+  .tree {
+    --json-tree-font-family: monospace;
+    --json-tree-font-size: 14px;
   }
 
   label {
@@ -105,14 +106,12 @@
   </div>
 </section>
 
-<section>
+<section class="tree">
   <label>G</label>
-  <pre class="json">{JSON.stringify(G, null, 2)}</pre>
+  <JSONTree value={G} />
 </section>
 
-<section>
+<section class="tree">
   <label>ctx</label>
-  <pre class="json">
-    {JSON.stringify(SanitizeCtx(ctx), null, 2)}
-  </pre>
+  <JSONTree value={SanitizeCtx(ctx)} />
 </section>

commit 28f20d71ba7151932d2c628b7b5631e90a2ccdf7

fix(debug): Fix runtime error introduced by #781 (#790)
* fix(debug): Import JSON tree Svelte component to fix runtime error

Relying on the compiled JavaScript of the JSON Tree component caused 
runtime errors. The best explanation I’ve read is here: 
<https://github.com/sveltejs/svelte/issues/3671#issuecomment-544329688>

* test(debug): Mock `svelte-json-tree-auto` in tests

`svelte-json-tree-auto` causes errors in tests (see #781). I think this 
might have something to do with the fact that the tree has circular 
dependencies, which perhaps the Jest transformer can’t handle. As it’s a 
third-party library, we can probably do without it during tests, and 
this change replaces it with an empty Svelte component for tests.

* test(debug): Add interactivity test for Debug panel

src/client/debug/main/Main.svelte
@@ -1,7 +1,7 @@
 <script>
   export let client;
 
-  const JSONTree = require('svelte-json-tree-auto');
+  import JSONTree from 'svelte-json-tree-auto/src/Root.svelte';
   import Move from './Move.svelte';
   import Controls from './Controls.svelte';
   import PlayerInfo from './PlayerInfo.svelte';

src/client/debug/tests/JSONTree.mock.svelte
@@ -1,0 +1,3 @@
+<script>
+  export let value;
+</script>

src/client/debug/tests/debug.test.js
@@ -7,7 +7,7 @@
  */
 
 import '@testing-library/jest-dom/extend-expect';
-import { render } from '@testing-library/svelte';
+import { render, fireEvent } from '@testing-library/svelte';
 import { Client } from '../../client';
 import Debug from '../Debug.svelte';
 
@@ -19,4 +19,22 @@ test('sanity', () => {
   expect(getByText('Players')).toBeInTheDocument();
   expect(getByText('G')).toBeInTheDocument();
   expect(getByText('ctx')).toBeInTheDocument();
+});
+
+test('switching panels', async () => {
+  const game = {};
+  const client = Client({ game });
+  const { getByText } = render(Debug, { props: { client } });
+
+  // switch to info tab
+  const InfoTab = getByText('Info');
+  await fireEvent.click(InfoTab);
+  expect(getByText('gameID')).toBeInTheDocument();
+  expect(getByText('playerID')).toBeInTheDocument();
+  expect(getByText('isActive')).toBeInTheDocument();
+
+  // switch to AI tab
+  const AITab = getByText('AI');
+  await fireEvent.click(AITab);
+  expect(getByText('No bots available.')).toBeInTheDocument();
 });